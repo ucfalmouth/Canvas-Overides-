c235ba8b02a50da93c1ac93b6a1c1927c00ddf9c
Damien Hogan <damienhogan87@gmail.com>
2015-01-20 15:54:59 +0000

    Settings as of January 2015

---
+++ stringoverrides.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	// Discussions 
+	$('#section-tabs').find('.discussions').text('My Studio');
+	$('#assignments_menu_item').find('.menu-item-no-drop').text('Past Assignments');
+	$('#section-tabs').find('.assignments').text('Submit My Assignment');
+	$('#section-tabs').find('.modules').text('My Modules');
+	$('#section-tabs').find('.announcements').text('My Announcements');
+	$('#section-tabs').find('.conferences').text('My Tutorials');
+	$('#section-tabs').find('.people').text('My Community');
+	$('#section-tabs').find('.collaborations').text('My Collaborations');
+});
+
